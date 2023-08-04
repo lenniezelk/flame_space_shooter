71b4d1cc97e63e5257c32cdd84fff5a31d9d1bf8
Leonard Kioi Kinyanjui <lenny.kinyanjui.lk@gmail.com>
2023-08-04 21:04:41 +0300

    change

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Flame Space Shooter
+# [WIP] Flame Space Shooter
 
 A side scrolling space shooter game built in Flutter and Flame. Animations in Rive.
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,3 @@
-import 'package:flame/extensions.dart';
-
 const playerSpeed = 300.0;
 const xPos = 20.0;
 const playerWidth = 100.0;
@@ -12,7 +10,3 @@ const projectileVelocity = 1000.0;
 const goLeft = -1.0;
 const goRight = 1.0;
 const enemySpeed = 150.0;
-const enemyColor = Color.fromARGB(255, 190, 234, 16);
-final enemyProjectileColor = enemyColor.brighten(0.2);
-const playerColor = Color.fromARGB(255, 240, 176, 15);
-final playerProjectileColor = playerColor.brighten(0.2);
